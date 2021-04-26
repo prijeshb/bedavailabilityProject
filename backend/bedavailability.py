17219199a4d29617919a3fb2c523f20db46e2014
Prijesh <prijeshbhingradiya1@gmail.com>
2021-04-26 18:07:32 +0530

    make it better

---
+++ backend/bedavailability.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import json
+import re
+import datetime
+import os
+curr = datetime.datetime.now()
+
+
+headers = {"User-Agent": "Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30"}
+response = requests.get("http://office.suratsmartcity.com/SuratCOVID19/Home/COVID19BedAvailabilitydetails?ftid=0", headers=headers)
+webpage = response.content
+
+soup = BeautifulSoup(webpage, "html.parser")
+hospital = "HOSPITAL: "
+hotel = "HOTEL"
+chc = "CHC"
+ccic = "CCIC"
+
+# table = soup.find('table',class_="BedAvailDashboard")
+# for tr in table.find_all('tr'):
+#     values = [data for data in tr.find_all('td')]
+#     if(values):
+#         catUpdateInfo = [ updata for updata in values[0].find_all('span')]
+#         category = catUpdateInfo[0]
+#         hospitalData['hospitalCategory'] = catUpdateInfo[0].text
+#         lastUpdatedAt = catUpdateInfo[1]
+#         hospitalData['hospitalLastUpdatedAt'] =  catUpdateInfo[1].text
+#         addressData = [a for a in values[0].find_all('a') ]
+#         miscHospitalData = addressData[0].get('href')
+#         cleanedHospitalData = miscHospitalData.split('showpopup')[1].strip('();').split("'")
+#         hospitalName = cleanedHospitalData[1]
+#         hospitalData['hospitalName'] = cleanedHospitalData[1]
+#         hospitalAddress = cleanedHospitalData[3]
+#         hospitalData['hospitalAddress'] = cleanedHospitalData[3]
+#         hospitalContactNo = cleanedHospitalData[5]
+#         hospitalData['hospitalContact'] = cleanedHospitalData[5]
+#         totalBeds = values[1].text
+#         hospitalData['hospitalTotalBeds'] = values[1].text
+#         availableBeds = values[2].text
+#         hospitalData['hospitalAvailableBeds'] = values[1].text
+#         bedData.append(hospitalData) 
+               # tr.find_all(class="hospital-info")
+                               #tr.find_all(class="count-text")
+bedData = []
+
+table = soup.find(id="accordionExample")
+for tr in table.find_all(class_="custom-card"):
+    hospitalData = {}
+    hospitalVacantBedTypes = []
+    miscHospitalData = tr.find_all(class_="hospital-info")[0].get('href')
+    cleanedHospitalData = miscHospitalData.split('showpopup')[1].strip('();').split("'")
+    hospitalData['hospitalName'] = cleanedHospitalData[1]
+    hospitalData['hospitalAddress'] = re.sub('\n',"",cleanedHospitalData[3])
+    hospitalData['hospitalContact'] = cleanedHospitalData[5]
+    hospitalData['hospitalLastUpdatedAt'] = tr.find_all(class_="badge-lastupdated")[0].text.split("-")[1]
+    hospitalData['hospitalAvailableBeds'] =  tr.find_all(class_="pr-2")[0].text.split("-")[1]
+    hospitalData['hospitalTotalBeds'] = tr.find_all(class_="count-text")[0].text.split("-")[1]
+
+    bedtypesList = tr.find(class_="card-body").find_all('li')
+    for bedTypeCont in bedtypesList:
+        hospitalBedType = {}
+        hospitalBedType['type'] = bedTypeCont.find(class_="caption-text").text
+        hospitalBedType['available'] = bedTypeCont.find(class_="count-text").text
+        hospitalVacantBedTypes.append(hospitalBedType)
+
+    hospitalData['hospitalVacantBedTypes'] = hospitalVacantBedTypes
+    
+    bedData.append(hospitalData)
+
+formattedYr =  "{}".format(curr.year)
+formattedat = ("{}".format(curr.day),"0{}".format(curr.day))[len(str(curr.day)) < 2 ] 
+formattedMo = ("{}".format(curr.month),"0{}".format(curr.month))[len(str(curr.month)) < 2]
+# if(0 <= curr.minute < 15):
+#     filestring = "" +  formattedat + formattedMo + formattedYr + ("{}".format(curr.hour),"0{}".format(curr.hour))[len(str(curr.hour)) < 2 ] + "00" + ".json"
+# elif 15 <= curr.minute < 30 :
+#     filestring = "" +  formattedat + formattedMo + formattedYr + ("{}".format(curr.hour),"0{}".format(curr.hour))[len(str(curr.hour)) < 2 ] + "15" + ".json" 
+# elif 30 <= curr.minute < 45:
+#     filestring = "" +  formattedat + formattedMo + formattedYr + ("{}".format(curr.hour),"0{}".format(curr.hour))[len(str(curr.hour)) < 2 ] + "30" + ".json"
+# else:
+#     filestring = "" +  formattedat + formattedMo + formattedYr + ("{}".format(curr.hour),"0{}".format(curr.hour))[len(str(curr.hour)) < 2 ] + "45" + ".json"
+filestring = "" +  formattedat + formattedMo + formattedYr + ("{}".format(curr.hour),"0{}".format(curr.hour))[len(str(curr.hour)) < 2 ] + ".json"
+filePath  = "" + "{}".format(formattedYr) + "{}{}".format(os.sep,formattedMo) + "{}{}".format(os.sep,formattedat)
+DIR_PATH = ".{}data{}".format(os.sep,os.sep) + filePath + "{}".format(os.sep)
+DATA_PATH = DIR_PATH + filestring
+
+if not os.path.isdir(DIR_PATH):
+    os.makedirs(DIR_PATH)
+
+with open('{}'.format(DATA_PATH),'w+') as f:
+     json.dump(bedData,f)
---
+++ backend/server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+
+app.use(cors())
+
+app.get("/",(req,res) => {
+    res.json({jsodn:"Hey there !!🥱"})
+})
+const convertToFormattedDigit  = (value) => {
+    let tempValue = null;
+    let prefix = "0";
+    if(!value)
+        throw Error("Passed value for formatting is undefined ");
+    
+    if(value.toString().length < 2 ){
+        tempValue = value
+        tempValue = prefix + value;
+        value = tempValue;
+    }
+    return value;
+}
+const getFormattedDateTimeValue = (currDateTime) => {
+    let formattedHr = currDateTime.getHours().toString().length < 2 ? convertToFormattedDigit(currDateTime.getHours()) : currDateTime.getHours();
+    let formattedDate = currDateTime.getDate().toString().length < 2 ? convertToFormattedDigit(currDateTime.getDate()) : currDateTime.getDate();
+    let formattedMont = currDateTime.getMonth().toString().length < 2 ? convertToFormattedDigit(currDateTime.getMonth()+1) : currDateTime.getMonth()+1;
+    return {formattedDate,formattedMont,formattedHr}
+}
+const getFileStringForCurrTime = () => {
+    let currDateTime = new Date();
+    let fileString = '';
+    let filePath = '';
+    let osPath = path.sep;
+    let dataPath = '';
+    let { formattedDate, formattedMont, formattedHr } = getFormattedDateTimeValue(currDateTime)
+    let currMin = currDateTime.getMinutes();
+    // if( 0 <= currMin  < 15)
+    //     fileString = `${formattedDate}${formattedMont}${currDateTime.getFullYear()}${formattedHr}00.json`
+    // else if( 15 <= currMin < 30)
+    //     fileString = `${formattedDate}${formattedMont}${currDateTime.getFullYear()}${formattedHr}15.json`
+    // else if( 30 <= currMin < 45)
+    //     fileString = `${formattedDate}${formattedMont}${currDateTime.getFullYear()}${formattedHr}30.json`   
+    // else if (45 <= currMin < 59)
+    //     fileString = `${formattedDate}${formattedMont}${currDateTime.getFullYear()}${formattedHr}45.json`
+        
+    fileString = `${formattedDate}${formattedMont}${currDateTime.getFullYear()}${formattedHr}.json`
+    filePath = `${currDateTime.getFullYear()}${osPath}${formattedMont}${osPath}${formattedDate}${osPath}`;
+    dataPath = `${osPath}data${osPath}` + filePath + fileString
+    return dataPath;
+}
+app.get("/availableBeds",(req,res) => {
+    let path = getFileStringForCurrTime();
+    if(!path)
+        path = `${osPath}data${osPath}`+  `default${path.sep}` + "default.json"
+    fs.readFile(`.${path}`,(err,data) => {
+        if(err) throw err
+        res.json(JSON.parse(data));
+    })
+})
+
+
+app.listen(process.env.PORT || 8090,()=>{
+    console.log('Listening on port 8090')
+})
